5c145a0fd68fb4b1dfc32af8db1c5ff111f8d199
ciro97 <39745066+ciro97@users.noreply.github.com>
2019-04-29 19:34:46 +0200

    boundle

--- app/load/load.js
+++ app/load/load.js
@@ -3,8 +3,8 @@ let native_zip = require("nativescript-zip").Zip;
 let fs = require("tns-core-modules/file-system");
 const httpModule = require("http");
 let appSetting = require("tns-core-modules/application-settings");
-let view;
 
+let view;
 let viewModel;
 let page;
 
@@ -12,33 +12,15 @@ function onNavigatingTo(args) {
     page = args.object;
 
     viewModel = observableModule.fromObject({});
+
     view = page.getViewById("toRotate");
+    view.animate({
+        rotate : 360,
+        duration : 2000,
+        iterations : Number.POSITIVE_INFINITY
+    });
 
     if(!fs.Folder.exists("/data/data/it.uniparthenope.museonavale/files/app/assets/zip")) {
-        /*let documents = fs.knownFolders.currentApp();
-        let zipFile = fs.path.join(fs.knownFolders.currentApp().path, "boundle.zip");
-        let dest = fs.path.join(fs.knownFolders.currentApp().path, "/assets/zip");
-
-        native_zip.unzipWithProgress(zipFile, dest, onZipProgress, true)
-            .then(() => {
-                console.log('unzip succesfully completed');
-                let url_main = documents.getFolder("/assets/zip/MuseoNavale");
-                url_main.getEntities().then(function (data) {
-                    for (let i = 1; i < data.length; i++) {
-                        let name = data[i]["_name"];
-
-                        if (url_main.getFile(name).extension == ".json") {
-                            appSetting.setString("fileJson", name);
-                            page.frame.navigate("home/home-page");
-                        }
-                    }
-                });
-            })
-            .catch(err => {
-                console.log('unzip error: ' + err);
-                fs.knownFolders.currentApp().getFolder("/assets/zip").remove();
-            });*/
-
         console.log('Download Started');
         viewModel.set("loading", "Downloading.....");
         let folder = fs.knownFolders.currentApp();
@@ -90,11 +72,6 @@ function onNavigatingTo(args) {
 function onZipProgress(args) {
     console.log('unzipping:' + args + "%");
     viewModel.set("loading", "Unzipping: " + args + " %");
-    view.animate({
-        rotate : 360,
-        duration : 2000,
-        iterations : Number.POSITIVE_INFINITY
-    });
 }
 
 exports.onNavigatingTo = onNavigatingTo;
