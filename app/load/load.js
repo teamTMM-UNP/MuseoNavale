2530858f2287fe9cf7157c889196dfda1fd1a32f
ciro97 <39745066+ciro97@users.noreply.github.com>
2019-04-29 18:38:24 +0200

    api request to download boundle

--- app/load/load.js
+++ app/load/load.js
@@ -15,7 +15,7 @@ function onNavigatingTo(args) {
     view = page.getViewById("toRotate");
 
     if(!fs.Folder.exists("/data/data/it.uniparthenope.museonavale/files/app/assets/zip")) {
-        let documents = fs.knownFolders.currentApp();
+        /*let documents = fs.knownFolders.currentApp();
         let zipFile = fs.path.join(fs.knownFolders.currentApp().path, "boundle.zip");
         let dest = fs.path.join(fs.knownFolders.currentApp().path, "/assets/zip");
 
@@ -37,19 +37,16 @@ function onNavigatingTo(args) {
             .catch(err => {
                 console.log('unzip error: ' + err);
                 fs.knownFolders.currentApp().getFolder("/assets/zip").remove();
-            });
+            });*/
 
-        /*console.log('Download Started');
+        console.log('Download Started');
         viewModel.set("loading", "Downloading.....");
         let folder = fs.knownFolders.currentApp();
         let file = fs.path.join(folder.path, "/assets/zip/prova.zip");
         let dest = fs.path.join(fs.knownFolders.currentApp().path, "/assets/zip");
-        let url = "http://www.mobilemind.com.br/makeyourself/coollife/images-2.1.zip";
+        let url = "http://museonavale.uniparthenope.it:5000/boundle";
         httpModule.getFile(url, file).then(function (r) {
             console.log(r.path);
-
-            viewModel.set("loading_height", "0");
-            viewModel.set("loading", "false");
 
             fs.knownFolders.currentApp().getFolder("/assets").getEntities().then(function (data) {
                 console.log(data);
@@ -58,19 +55,27 @@ function onNavigatingTo(args) {
             native_zip.unzipWithProgress(file, dest, onZipProgress, true)
                 .then(() => {
                     console.log('unzip succesfully completed');
-                    let url_main = folder.getFolder("/assets/zip");
+                    let url_main = folder.getFolder("/assets/zip/MuseoNavale");
                     url_main.getEntities().then(function (data) {
+                        for (let i = 1; i < data.length; i++) {
+                            let name = data[i]["_name"];
 
+                            if (url_main.getFile(name).extension == ".json") {
+                                appSetting.setString("fileJson", name);
+                                page.frame.navigate("home/home-page");
+                            }
+                        }
                     });
-                }).catch(err => {
-                console.log('unzip error: ' + err);
-            });
+                })
+                .catch(err => {
+                    console.log('unzip error: ' + err);
+                    fs.knownFolders.currentApp().getFolder("/assets/zip").remove();
+                });
+            },function (e) {
+                console.log("Errore: " + e);
+        });
 
-        }, function (e) {
-            //// Argument (e) is Error!
-        });*/
     }
-
     else{
         const navigationEntry = {
             moduleName: "intro/intro",
