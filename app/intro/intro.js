450553c42b709a7e1ec5f9699f6e8fd5ba726c2f
ciro97 <39745066+ciro97@users.noreply.github.com>
2019-05-03 16:18:33 +0200

    ios

--- app/intro/intro.js
+++ app/intro/intro.js
@@ -26,49 +26,98 @@ function onNavigatingTo(args) {
 }
 
 function benvenuto(){
-    if(!fs.Folder.exists("/data/data/it.uniparthenope.museonavale/files/app/assets/zip/file/MuseoNavale")) {
-        page.frame.navigate("load/load");
-    }
-    else{
-        const myConnectionType = connectivityModule.getConnectionType();
-        console.log(myConnectionType);
-        if(myConnectionType == "1" || myConnectionType == "2"){
-            fetch("http://museonavale.uniparthenope.it:5000/version").then((response) => response.json()).then((data) =>{
-                let documents = fs.knownFolders.currentApp();
-                let url_main = documents.getFolder("/assets/zip/file/MuseoNavale");
-                let fileJson = url_main.getFile(appSetting.getString("fileJson"));
-                fileJson.readText().then(function (data1) {
-                    let jsonData = JSON.parse(data1);
-                    let version = jsonData["version"];
-                    let version_web = data.version;
-                    console.log("Version Web: " + version_web);
-                    console.log("My Version : " + version);
-                    if(version != version_web){
-                        dialogs.confirm({
-                            title: "Aggiornamento!!",
-                            message: "E' disponibile un nuovo aggiornamento. Scaricarlo?",
-                            okButtonText: "Si",
-                            cancelButtonText: "No"
-                        }).then(function (result) {
-                            console.log("Dialog result: " + result);
-                            if(result){
-                                appSetting.setString("update", "YES");
-                                page.frame.navigate("load/load");
-                            }
-                            else
-                                page.frame.navigate("home/home-page");
-                        });
-                    }
-                    else{
-                        page.frame.navigate("home/home-page");
-                    }
-                });
-            }).catch(error => {
-                page.frame.navigate("home/home-page");
-            });
+    if(device.isAndroid){
+        if(!fs.Folder.exists("/data/data/it.uniparthenope.museonavale/files/app/assets/zip/file/MuseoNavale")) {
+            page.frame.navigate("load/load");
         }
-        else {
-            page.frame.navigate("home/home-page");
+        else{
+            const myConnectionType = connectivityModule.getConnectionType();
+            console.log(myConnectionType);
+            if(myConnectionType == "1" || myConnectionType == "2"){
+                fetch("http://museonavale.uniparthenope.it:5000/version").then((response) => response.json()).then((data) =>{
+                    let documents = fs.knownFolders.currentApp();
+                    let url_main = documents.getFolder("/assets/zip/file/MuseoNavale");
+                    let fileJson = url_main.getFile(appSetting.getString("fileJson"));
+                    fileJson.readText().then(function (data1) {
+                        let jsonData = JSON.parse(data1);
+                        let version = jsonData["version"];
+                        let version_web = data.version;
+                        console.log("Version Web: " + version_web);
+                        console.log("My Version : " + version);
+                        if(version != version_web){
+                            dialogs.confirm({
+                                title: "Aggiornamento!!",
+                                message: "E' disponibile un nuovo aggiornamento. Scaricarlo?",
+                                okButtonText: "Si",
+                                cancelButtonText: "No"
+                            }).then(function (result) {
+                                console.log("Dialog result: " + result);
+                                if(result){
+                                    appSetting.setString("update", "YES");
+                                    page.frame.navigate("load/load");
+                                }
+                                else
+                                    page.frame.navigate("home/home-page");
+                            });
+                        }
+                        else{
+                            page.frame.navigate("home/home-page");
+                        }
+                    });
+                }).catch(error => {
+                    page.frame.navigate("home/home-page");
+                });
+            }
+            else {
+                page.frame.navigate("home/home-page");
+            }
+        }
+    }
+    else if(device.isIOS){
+        if(!fs.Folder.exists("/Library/Application Support/LiveSync/app/assets/zip/file/MuseoNavale")){
+            page.frame.navigate("load/load");
+        }
+        else{
+            const myConnectionType = connectivityModule.getConnectionType();
+            console.log(myConnectionType);
+            if(myConnectionType == "1" || myConnectionType == "2"){
+                fetch("http://museonavale.uniparthenope.it:5000/version").then((response) => response.json()).then((data) =>{
+                    let documents = fs.knownFolders.currentApp();
+                    let url_main = documents.getFolder("/assets/zip/file/MuseoNavale");
+                    let fileJson = url_main.getFile(appSetting.getString("fileJson"));
+                    fileJson.readText().then(function (data1) {
+                        let jsonData = JSON.parse(data1);
+                        let version = jsonData["version"];
+                        let version_web = data.version;
+                        console.log("Version Web: " + version_web);
+                        console.log("My Version : " + version);
+                        if(version != version_web){
+                            dialogs.confirm({
+                                title: "Aggiornamento!!",
+                                message: "E' disponibile un nuovo aggiornamento. Scaricarlo?",
+                                okButtonText: "Si",
+                                cancelButtonText: "No"
+                            }).then(function (result) {
+                                console.log("Dialog result: " + result);
+                                if(result){
+                                    appSetting.setString("update", "YES");
+                                    page.frame.navigate("load/load");
+                                }
+                                else
+                                    page.frame.navigate("home/home-page");
+                            });
+                        }
+                        else{
+                            page.frame.navigate("home/home-page");
+                        }
+                    });
+                }).catch(error => {
+                    page.frame.navigate("home/home-page");
+                });
+            }
+            else {
+                page.frame.navigate("home/home-page");
+            }
         }
     }
 }
